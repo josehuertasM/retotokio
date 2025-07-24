252a8dd38cdf5cff4578b7039b3242b1aa8edfef
Jose huertas medrano <josehuertasmedrano3@gmail.com>
2025-07-23 23:35:15 -0500

    second commit

---
+++ src/main/java/com/example/retotokio/RetotokioApplication.java
@@ -0,0 +1,13 @@
+package com.example.retotokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetotokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetotokioApplication.class, args);
+	}
+
+}
